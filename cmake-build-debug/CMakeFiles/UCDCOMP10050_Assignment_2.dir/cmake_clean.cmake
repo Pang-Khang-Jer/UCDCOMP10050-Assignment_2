d8f56c31fba26199fd3860751c265e7e9b57f998
Pang Khang Jer <khang.pang@ucdconnect.ie>
2021-04-09 18:36:56 +0800

    created the project

---
+++ cmake-build-debug/CMakeFiles/UCDCOMP10050_Assignment_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UCDCOMP10050_Assignment_2.dir/main.c.obj"
+  "UCDCOMP10050_Assignment_2.exe"
+  "UCDCOMP10050_Assignment_2.exe.manifest"
+  "UCDCOMP10050_Assignment_2.pdb"
+  "libUCDCOMP10050_Assignment_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/UCDCOMP10050_Assignment_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
